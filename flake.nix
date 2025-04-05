25712fa829e6d57d1aebe3f44cb128ef10acb2c7
tomiock <ockier1@gmail.com>
2025-04-05 14:16:10 +0200

    problems witht the confusion matrix

--- flake.nix
+++ flake.nix
@@ -60,6 +60,7 @@ EOF
                         pythonPackages.matplotlib
                         pythonPackages.numpy
                         pythonPackages.pandas
+                        pythonPackages.seaborn
                         pythonPackages.spacy
                         pythonPackages.nltk
                         pythonPackages.pytest
